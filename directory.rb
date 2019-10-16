b3ab9776dad986c591e274aad5bb39bf30f9a83a
Muhammad Subhan <subhanansari586@gmail.com>
2019-10-16 17:18:02 +0100

    Using method to display students' details

--- directory.rb
+++ directory.rb
@@ -13,11 +13,19 @@ students = [
 "Norman Bates",
 "Muhammad Subhan"
 ]
-# and then print them
+def print_header
 puts "The students of Villains Academy"
 puts "-------------"
-students.each do |student|
-  puts student
 end
-# finally, we print the total
-puts "Overall, we have #{students.count} great students"
+def print(names)
+names.each do |name|
+  puts name
+end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
