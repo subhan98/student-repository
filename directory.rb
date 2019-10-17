1e3985a1cb6f0217ecdf0c5d23652a165757b534
Muhammad Subhan <subhanansari586@gmail.com>
2019-10-17 08:57:52 +0100

    User Interaction

--- directory.rb
+++ directory.rb
@@ -1,3 +1,45 @@
+def input_students
+   puts "Plese enter the names of the students"
+   puts "To finish, just enter hit twice"
+   #create and empty array
+   students = []
+   #get the first name
+   name = gets.chomp
+   # while the name is not empty, repeat this code
+   while !name.empty? do
+     # add the student hash to the array
+     students << {name: name,cohort: :november}
+     puts "Now we have #{students.count} students"
+      # get another name from the user
+      name = gets.chomp
+    end
+    #return the array of students
+    students
+  end
+
+def print_header
+puts "The students of the Makers Academy"
+puts "-------------"
+end
+def print(students)
+students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
+
+
+
+=begin
 # let's put all students into an array
 students = [
   {name: "Dr. Hannibal Lector", cohort: :november},
@@ -13,20 +55,4 @@ students = [
   {name: "Norman Bates", cohort: :november}
 
 ]
-def print_header
-puts "The students of the Makers Academy"
-puts "-------------"
-end
-def print(students)
-students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
-end
-end
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-# nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+=end
